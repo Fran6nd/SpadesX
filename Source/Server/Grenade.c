1cd708cbd3b4c96982212ef2cfd234deb8c9d35f
Francois ND <francoisnd1@gmail.com>
2026-03-20 22:02:50 +0100

    scripting: extract grenade_explode_at from handle_grenade
    Pull the explosion body (block action, player damage, block destruction,
    physics node check, intel/tent drop) into a standalone
    grenade_explode_at(server, player, pos) so the Lua layer can trigger a
    real grenade explosion at an arbitrary position.
    
    handle_grenade now calls grenade_explode_at after the scripting deny check.

--- Source/Server/Grenade.c
+++ Source/Server/Grenade.c
@@ -55,6 +55,64 @@ uint8_t get_grenade_damage(server_t* server, player_t* damaged_player, grenade_t
     return 0;
 }
 
+void grenade_explode_at(server_t* server, player_t* player, vector3f_t pos)
+{
+    uint8_t allowToDestroy = 0;
+    if (grenadeGamemodeCheck(server, pos)) {
+        send_block_action(server, player, 3, floor(pos.x), floor(pos.y), floor(pos.z));
+        allowToDestroy = 1;
+    }
+    grenade_t fake = {0};
+    fake.position  = pos;
+    player_t *connected_player, *tmp_player;
+    HASH_ITER(hh, server->players, connected_player, tmp_player)
+    {
+        if (connected_player->state == STATE_READY) {
+            uint8_t value = get_grenade_damage(server, connected_player, &fake);
+            if (value > 0) {
+                send_set_hp(server, player, connected_player, value, 1, 3, 5, 1, pos);
+            }
+        }
+    }
+    float x = pos.x;
+    float y = pos.y;
+    for (int z = (int)pos.z - 1; z <= (int)pos.z + 1; ++z) {
+        if (z < 62 &&
+            (x >= 0 && x <= server->s_map.map.size_x && x - 1 >= 0 && x - 1 <= server->s_map.map.size_x &&
+             x + 1 >= 0 && x + 1 <= server->s_map.map.size_x) &&
+            (y >= 0 && y <= server->s_map.map.size_y && y - 1 >= 0 && y - 1 <= server->s_map.map.size_y &&
+             y + 1 >= 0 && y + 1 <= server->s_map.map.size_y))
+        {
+            if (allowToDestroy && (z >= 0 && z < server->s_map.map.size_z)) {
+                // This is cause casting float to int produces an edge case where
+                // float < 0 rounds to value closer to 0. Which for -0.(>0) is bad
+                int x_rounded = floorf(x - 1);
+                int y_rounded = floorf(y - 1);
+                for (int X = x_rounded; X < x_rounded + 3; ++X) {
+                    for (int Y = y_rounded; Y < y_rounded + 3; ++Y)
+                    { // I hate nested loops as any other C dev but here they do not cost that much perf
+                        if (valid_pos_3f(server, X, Y, z))
+                            mapvxl_set_air(&server->s_map.map, X, Y, z);
+                    }
+                }
+            }
+            vector3i_t pos_int;
+            pos_int.x = (int)pos.x;
+            pos_int.y = (int)pos.y;
+            pos_int.z = (int)pos.z;
+
+            vector3i_t* neigh = getGrenadeNeighbors(pos_int);
+
+            for (int index = 0; index < 54; ++index) {
+                if (valid_pos_v3i(server, neigh[index])) {
+                    check_node(server, neigh[index]);
+                }
+            }
+        }
+    }
+    move_intel_and_tent_down(server);
+}
+
 void handle_grenade(server_t* server, player_t* player)
 {
     grenade_t* grenade;
@@ -76,69 +134,11 @@ void handle_grenade(server_t* server, player_t* player)
             if ((get_nanos() - grenade->time_since_sent) / 1000000000.f >= grenade->fuse) {
                 int denied = (scripting_on_grenade_explode(server, player, grenade->position) == SCRIPTING_DENY);
                 if (!denied) {
-                    uint8_t allowToDestroy = 0;
-                    if (grenadeGamemodeCheck(server, grenade->position)) {
-                        send_block_action(server,
-                                          player,
-                                          3,
-                                          floor(grenade->position.x),
-                                          floor(grenade->position.y),
-                                          floor(grenade->position.z));
-                        allowToDestroy = 1;
-                    }
-                    player_t *connected_player, *tmp_player;
-                    HASH_ITER(hh, server->players, connected_player, tmp_player)
-                    {
-                        if (connected_player->state == STATE_READY) {
-                            uint8_t value = get_grenade_damage(server, connected_player, grenade);
-                            if (value > 0) {
-                                send_set_hp(server, player, connected_player, value, 1, 3, 5, 1, grenade->position);
-                            }
-                        }
-                    }
-                    float x = grenade->position.x;
-                    float y = grenade->position.y;
-                    for (int z = grenade->position.z - 1; z <= grenade->position.z + 1; ++z) {
-                        if (z < 62 &&
-                            (x >= 0 && x <= server->s_map.map.size_x && x - 1 >= 0 && x - 1 <= server->s_map.map.size_x &&
-                             x + 1 >= 0 && x + 1 <= server->s_map.map.size_x) &&
-                            (y >= 0 && y <= server->s_map.map.size_y && y - 1 >= 0 && y - 1 <= server->s_map.map.size_y &&
-                             y + 1 >= 0 && y + 1 <= server->s_map.map.size_y))
-                        {
-                            if (allowToDestroy && (z >= 0 && z < server->s_map.map.size_z)) {
-                                // This is cause casting float to int produces an edge case where
-                                // float < 0 rounds to value closer to 0. Which for -0.(>0) is bad
-                                int x_rounded = floorf(x - 1);
-                                int y_rounded = floorf(y - 1);
-                                for (int X = x_rounded; X < x_rounded + 3; ++X) {
-                                    for (int Y = y_rounded; Y < y_rounded + 3; ++Y)
-                                    { // I hate nested loops as any other C dev but here they do not cost that much perf
-                                        if (valid_pos_3f(server, X, Y, z))
-                                            mapvxl_set_air(&server->s_map.map, X, Y, z);
-                                    }
-                                }
-                            }
-                            vector3i_t pos;
-                            pos.x = grenade->position.x;
-                            pos.y = grenade->position.y;
-                            pos.z = grenade->position.z;
-
-                            vector3i_t* neigh = getGrenadeNeighbors(pos);
-
-                            for (int index = 0; index < 54; ++index) {
-                                if (valid_pos_v3i(server, neigh[index])) {
-                                    check_node(server, neigh[index]);
-                                }
-                            }
-                        }
-                    }
+                    grenade_explode_at(server, player, grenade->position);
                 }
                 grenade->sent = 0;
                 DL_DELETE(player->grenade, grenade);
                 free(grenade);
-                if (!denied) {
-                    move_intel_and_tent_down(server);
-                }
             }
         }
     }
--- Source/Server/Grenade.h
+++ Source/Server/Grenade.h
@@ -5,6 +5,7 @@
 
 vector3i_t* getGrenadeNeighbors(vector3i_t pos);
 uint8_t     get_grenade_damage(server_t* server, player_t* damaged_player, grenade_t* grenade);
+void        grenade_explode_at(server_t* server, player_t* player, vector3f_t pos);
 void        handle_grenade(server_t* server, player_t* player);
 
 #endif
